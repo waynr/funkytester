0f7ae4664d40c6f3b2cb458109f8b6179a0754de
Wayne Warren <waynr@sdf.org>
2013-07-10 12:09:43 -0500

    server: Begin refactoring PlatformServers
     * Need to simplify individual PlatformServer implementations by factoring out
       common functionality; this issue came up due to the fact that the sockets
       implementation had a great deal of copy-and-pasted code from the process
       implementation.
    
    Signed-off-by: Wayne Warren <waynr@sdf.org>

--- lib/ft/server/process.py
+++ lib/ft/server/process.py
@@ -7,33 +7,16 @@ import signal, logging, threading, os
 
 import ft.event
 from ft.platform import Platform
+from ft.server.common import PlatformClient, EventHandlerRegistry
 
-class PlatformServerConnection(threading.Thread):
+class PlatformProcessClient(PlatformClient):
 
-    def __init__(self, server_info):
-        super(PlatformServerConnection, self).__init__()
+    def __init__(self, server_info, *args, **kwargs):
+        super(PlatformProcessClient, self).__init__(*args, **kwargs)
 
         self.channel = server_info[0]
-        self.handler_registry = PlatformEventHandlerRegistry()
 
-        self.outgoing_queue = Queue()
-        self.incoming_queue = Queue()
-
-        self.running = False
-        self.setDaemon = True
-
-    def run(self):
-        self.running = True
-        self.main()
-
-    def main(self):
-        while self.running:
-            message = self.__receive_message()
-            if message:
-                self.__handle_message(message)
-            self.__handle_outgoing_queue()
-
-    def __receive_message(self):
+    def _receive_message(self):
         try:
             if self.channel.poll():
                 message = self.channel.recv()
@@ -41,7 +24,7 @@ class PlatformServerConnection(threading.Thread):
         except Exception:
             return None, "ERROR: Problem receiving command response."
 
-    def __handle_message(self, message):
+    def _handle_message(self, message):
         if isinstance(message, ft.event.Event):
             logging.debug(message)
             self.handler_registry.fire(message)
@@ -51,36 +34,17 @@ class PlatformServerConnection(threading.Thread):
             logging.error("Unhandled PlatformServer message:" 
                     " {0}".format(message))
 
-    def __get_outgoing_item(self):
+    def _get_outgoing_item(self):
         try:
             return self.outgoing_queue.get(False)
         except StdLibQueue.Empty:
             return None
 
-    def __handle_outgoing_queue(self):
-        command = self.__get_outgoing_item()
+    def _handle_outgoing_queue(self):
+        command = self._get_outgoing_item()
         if command:
             self.channel.send(command)
 
-    def run_command(self, command):
-        self.outgoing_queue.put(command)
-        if command == "TERMINATE":
-            return None, ""
-        try:
-            response =  self.incoming_queue.get(True, 20)
-            if response:
-                return response
-            else:
-                return None, "FATAL: Timeout while waiting for response from PlatformServer."
-        except KeyboardInterrupt:
-            pass
-
-    def register_handler(self, handler):
-        self.handler_registry.register_handler(handler)
-
-    def terminate(self):
-        self.run_command("TERMINATE")
-        
 class PlatformProcessServer(Process):
 
     def __init__(self, server_channel):
@@ -93,7 +57,7 @@ class PlatformProcessServer(Process):
         self.platform = None # is set externally
 
         self.outgoing_queue = Queue()
-        self.event_registry = PlatformEventHandlerRegistry()
+        self.event_registry = EventHandlerRegistry()
 
     def run(self):
         self.running = True
@@ -148,32 +112,6 @@ class PlatformProcessServer(Process):
         self.channel.close()
         self.platform.cleanup()
 
-## Keep track of event handlers (remote and local) for event distribution.
-#
-class PlatformEventHandlerRegistry(object):
-
-    def __init__(self):
-        self._temp_queue = Queue()
-        self.handlers = list()
-
-    def fire(self, event):
-        if len(self.handlers) == 0:
-            self._temp_queue.append(event)
-            return
-
-        for handler in self.handlers:
-            handler.notify(event)
-
-    def register_handler(self, handler):
-        self.handlers.append(handler)
-        return True
-
-    def unregister_handler(self, handler):
-        if handler in self.handlers:
-            self.handlers.remove(handler)
-            return True
-        return False
-
 ## PlatformServer is an interface wraps some type of server to provide a
 #  consistent API during program startup so that different server
 #  implementations can be used with relatively little difficulty using the same
@@ -205,7 +143,7 @@ class PlatformServer(object):
     ## Initiate and return connection to a remote PlatformServer.
     #
     def establish_connection(self, serverinfo=None):
-        self.connection = PlatformServerConnection(self.serverinfo)
+        self.connection = PlatformProcessClient(self.serverinfo)
         return self.connection
 
     ## Launch given UI main() with the given args.
